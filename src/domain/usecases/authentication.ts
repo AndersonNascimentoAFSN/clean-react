e46db4a82177b39e3119e5945a58739045a5e0c4
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-08-21 19:38:52 -0300

    refactor: change imports to use module

--- src/domain/usecases/authentication.ts
+++ src/domain/usecases/authentication.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import type { AccountModel } from 'domain/models/account-model'
+import type { AccountModel } from '@/domain/models/account-model'
 
 export type AuthenticationParams = {
   email: string
