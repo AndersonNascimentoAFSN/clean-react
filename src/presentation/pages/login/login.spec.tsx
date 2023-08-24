7c0e924147da1a3e53c6a7a9b2206caa21a048fe
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-08-24 16:52:34 -0300

    test: ensure Login starts with submit button disabled

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,19 +1,27 @@
 import React from 'react'
 
-import { render } from '@testing-library/react'
+import { getByRole, render } from '@testing-library/react'
 
 import { Login } from './login'
 
 describe('Login Component', () => {
-  it('should not render spinner and error message on start', () => {
-    const { getByTestId, queryByTestId } = render(<Login />)
-    // const errorWrap = getByTestId('form-status')
-    // expect(errorWrap.childElementCount).toBe(0)
+  describe('should start with initial state', () => {
+    it('should not render spinner and error message on start', () => {
+      const { getByTestId, queryByTestId } = render(<Login />)
+      // const errorWrap = getByTestId('form-status')
+      // expect(errorWrap.childElementCount).toBe(0)
 
-    const spinner = queryByTestId('spinner')
-    const errorMessage = queryByTestId('error-message')
+      const spinner = queryByTestId('spinner')
+      const errorMessage = queryByTestId('error-message')
 
-    expect(spinner).not.toBeInTheDocument()
-    expect(errorMessage).not.toBeInTheDocument()
+      expect(spinner).not.toBeInTheDocument()
+      expect(errorMessage).not.toBeInTheDocument()
+    })
+    it('should starts with a submit button disabled', () => {
+      const { getByRole } = render(<Login />)
+
+      const submitButton = getByRole('button', { name: /entrar/i })
+      expect(submitButton).toHaveAttribute('disabled')
+    })
   })
 })
