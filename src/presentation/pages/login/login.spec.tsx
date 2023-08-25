8aa7e8af40af7460000f747699f9d3e45d65b0d6
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-08-25 11:18:14 -0300

    test: ensure lLogin start with inputs with required state

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -23,5 +23,25 @@ describe('Login Component', () => {
       const submitButton = getByRole('button', { name: /entrar/i })
       expect(submitButton).toHaveAttribute('disabled')
     })
+    // it('should starts with a submit button disabled', () => {
+    //   const { getByRole } = render(<Login />)
+
+    //   const emailInput = getByRole('textbox', { name: /email/i })
+    //   const passwordInput = getByRole('textbox', { name: /password/i })
+
+    //   expect(emailInput).toBeInTheDocument()
+    //   expect(passwordInput).toBeInTheDocument()
+    // })
+    it('should starts input error span with title and content initial', () => {
+      const { getByTestId } = render(<Login />)
+
+      const emailStatus = getByTestId('email-status')
+      const passwordStatus = getByTestId('password-status')
+
+      expect(emailStatus.title).toBe('Campo obrigatório')
+      expect(passwordStatus.title).toBe('Campo obrigatório')
+      expect(emailStatus).toHaveTextContent('🔴')
+      expect(passwordStatus).toHaveTextContent('🔴')
+    })
   })
 })
